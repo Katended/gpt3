d6621b42d559b4374b3c6a44b3c9ae4d78564234
David Katende <katendedavidjoga@gmail.com>
2023-08-26 12:34:28 +0300

    Add features component

--- src/containers/features/features.jsx
+++ src/containers/features/features.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import Feature from '../../components/feature/feature'
 import './features.css'
 
-const Features = () => {
-  return (
-    <div>
-      Feature
-    </div>
-  )
-}
+const featuresData = [
+  {
+    title: 'Text',
+    text: 'Text models are advanced language processing tools that can generate, classify, and summarize text with high levels of coherence and accuracy.',
+  },
+  {
+    title: 'Image',
+    text: 'Research on generative modeling for images has led to representation models like CLIP, which makes a map between text and images that an AI can read, and DALL-E, a tool for creating vivid images from text descriptions.',
+  },
+  {
+    title: 'Audio',
+    text: 'Research on applying AI to audio processing and audio generation has led to developments in automatic speech recognition and original musical compositions.',
+  }
+];
 
+const Features = () => (
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-heading">
+     <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>     
+    </div>
+    <div className="gpt3__features-container">
+      {featuresData.map((item, index) => {
+        return (
+          <Feature title={item.title} text={item.text} key={item.title + index} />
+        );
+      })}
+    </div>
+  </div>
+);
 export default Features
--- src/containers/whatgpt3/whatgpt3.jsx
+++ src/containers/whatgpt3/whatgpt3.jsx
@@ -4,14 +4,13 @@ import Feature from '../../components/feature/feature'
 
 const Whatgpt3 = () => {
   return (
-    <div className='gpt3__whatgpt3 section__margin' id="gpt3">
+    <div className='gpt3__whatgpt3 section__margin' id="whatgpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature title="What is GPT3?" text="GPT-3's versatility allows it to be used for a wide range of applications across various domains. Here are some examples of what can be built using GPT-3"/>
+        <Feature title="What is GPT3?" text="GPT-3, which stands for Generative Pre-trained Transformer 3,is a state-of-the-art language processing AI model developed by OpenAI. It is the third iteration of the GPT series and is known for its remarkable language generation and understanding capabilities. GPT-3 is designed based on a transformer architecture, which is a type of neural network architecture specifically tailored for handling sequential data, such as text."/>
       </div>
       <div className='gpt3__whatgpt3-heading'>
         <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
-        <p>Explore The Library</p>
-      </div>
+       </div>
       <div className="gpt3__whatgpt3-container">       
         <Feature title='Text Generation' text='GPT-3 can generate human-like text for creative writing, storytelling, content creation, and more. It can write articles, poems, stories, and even dialogue for characters in a narrative.'/>
         <Feature title='Language Translation' text='GPT-3 can be used for translating text between different languages. While it might not be as accurate as specialized translation models, it can still provide decent translations.'/>
