00498ad2835fdbb5222d413ed8448ec37a12c667
David Katende <katendedavidjoga@gmail.com>
2023-08-27 17:17:52 +0300

    Finish coponents

--- src/components/cta/cta.jsx
+++ src/components/cta/cta.jsx
@@ -3,8 +3,13 @@ import './cta.css';
 
 const Cta = () => {
   return (
-    <div>
-      Cta
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>      
+        <h3> Register Today & start exploring</h3>
+      </div>
+      <div className='gpt3__cta-btn'>
+        <button type='button'>Get Started</button>
+      </div>
     </div>
   )
 }
